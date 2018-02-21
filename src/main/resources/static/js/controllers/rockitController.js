bbed78a73e7f5dd188f6e1b2d89a72296388fe76
kkostin <kkostin@rocketsoftware.com>
2018-02-21 17:10:29 +0500

    Add angular and bootstrap

---
+++ src/main/resources/static/js/controllers/rockitController.js
@@ -0,0 +1,8 @@
+app.controller('rockitController', ['$scope', '$q', '$log', '$window', '$timeout', '$http', '$interval', '$rootScope',
+    function AdminController($scope, $q, $log, $window, $timeout, $http, $interval, $rootScope) {
+
+
+    }
+]);
+
+
