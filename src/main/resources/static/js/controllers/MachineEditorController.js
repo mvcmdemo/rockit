d37ebef6b6844d7a3291a8a9f47ded40812585b1
kkostin <kkostin@rocketsoftware.com>
2018-02-24 00:37:41 +0500

    Add new modal Machine Configuration; UI changes

---
+++ src/main/resources/static/js/controllers/MachineEditorController.js
@@ -0,0 +1,33 @@
+app.controller('MachineEditorController', function ($scope, $uibModalInstance, $timeout, machine, machines, OSs, groups) {
+    $scope.machine = angular.copy(machine);
+    $scope.machines = angular.copy(machines);
+    $scope.OSs = angular.copy(OSs);
+    $scope.groups = angular.copy(groups);
+
+    $scope.toggleGroup = function(group) {
+        var idx = $scope.machine.groups.indexOf(group);
+        // if group exists in machine.groups then we should remove it
+        if (idx > -1) {
+            $scope.machine.groups.splice(idx, 1);
+        }
+        // else, add new group
+        else {
+            $scope.machine.groups.push(groups);
+        }
+    };
+
+    function validateMachine() {
+        return true;
+    }
+
+    $scope.save = function() {
+        if (!validateMachine()) {
+            return;
+        }
+        $uibModalInstance.close($scope.machine);
+    };
+
+    $scope.cancel = function(){
+        $uibModalInstance.dismiss('cancel');
+    }
+});
--- src/main/resources/static/js/controllers/rockitController.js
+++ src/main/resources/static/js/controllers/rockitController.js
@@ -1,6 +1,63 @@
-app.controller('rockitController', ['$scope', '$q', '$log', '$window', '$timeout', '$http', '$interval', '$rootScope',
-    function AdminController($scope, $q, $log, $window, $timeout, $http, $interval, $rootScope) {
+app.controller('rockitController', ['$scope', '$q', '$log', '$window', '$timeout', '$http', '$interval', '$rootScope', '$uibModal',
+    function AdminController($scope, $q, $log, $window, $timeout, $http, $interval, $rootScope, $uibModal) {
 
+        $scope.animationsEnabled = true;
+
+        $scope.OSs = ['Windows', 'Unix'];
+
+        $scope.machines = [];
+        $scope.groups = ['SBXA', 'MV', 'CorVU', 'RMob'];
+
+        $scope.addMachine = function() {
+            var machine = {
+                name: '',
+                hostname: '',
+                username: '',
+                password: '',
+                os: '',
+                description: '',
+                groups: []
+            };
+            openMachineEditor(machine, true);
+        };
+
+        function openMachineEditor(machine, isNew) {
+            var oldMachineName = machine.name;
+            var modalInstance = $uibModal.open({
+                backdrop : false,
+                animation : $scope.animationsEnabled,
+                templateUrl : 'views/machine.html',
+                controller : 'MachineEditorController',
+                size : 'lg',
+                resolve : {
+                    machine : function () {
+                        return machine;
+                    },
+                    machines : function () {
+                        return $scope.machines;
+                    },
+                    OSs : function () {
+                        return $scope.OSs;
+                    },
+                    groups : function () {
+                        return $scope.groups;
+                    }
+
+                }
+            });
+            modalInstance.result.then(function (returnMachine) {
+                if (isNew) {
+                    // add a new machine
+
+                }
+                else{
+                    // edit an existing machine
+
+                }
+            }, function () {
+                return false;
+            });
+        }
 
     }
 ]);
