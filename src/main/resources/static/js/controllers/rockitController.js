f26937796d580107feda341139f9f1601c59e71d
mvcmdemo <kkostin@rocketsoftware.com>
2018-06-14 17:12:49 +0500

    Changed the way of sorting the tables

--- src/main/resources/static/js/controllers/rockitController.js
+++ src/main/resources/static/js/controllers/rockitController.js
@@ -14,6 +14,12 @@ app.controller('rockitController', ['$scope', '$q', '$log', '$window', '$timeout
 
         editableOptions.theme = 'bs3';
 
+        $scope.machineSortType = "state";
+        $scope.machineSortReverse = "false";
+
+        $scope.userSortType = "name";
+        $scope.userSortReverse = "false";
+
         $scope.users = [];
 
         $scope.user = {login: 'admin', password: 'admin', admin: true, groups: []};
@@ -307,61 +313,6 @@ app.controller('rockitController', ['$scope', '$q', '$log', '$window', '$timeout
             });
 
         };
-
-        $scope.sortTable = function (tableId, n) {
-            var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-            table = document.getElementById(tableId);
-            switching = true;
-            // Set the sorting direction to ascending:
-            dir = "asc";
-            /* Make a loop that will continue until
-            no switching has been done: */
-            while (switching) {
-                // Start by saying: no switching is done:
-                switching = false;
-                rows = table.getElementsByTagName("TR");
-                /* Loop through all table rows (except the
-                first, which contains table headers): */
-                for (i = 1; i < (rows.length - 1); i++) {
-                    // Start by saying there should be no switching:
-                    shouldSwitch = false;
-                    /* Get the two elements you want to compare,
-                    one from current row and one from the next: */
-                    x = rows[i].getElementsByTagName("TD")[n];
-                    y = rows[i + 1].getElementsByTagName("TD")[n];
-                    /* Check if the two rows should switch place,
-                    based on the direction, asc or desc: */
-                    if (dir == "asc") {
-                        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                            // If so, mark as a switch and break the loop:
-                            shouldSwitch = true;
-                            break;
-                        }
-                    } else if (dir == "desc") {
-                        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-                          // If so, mark as a switch and break the loop:
-                          shouldSwitch = true;
-                          break;
-                        }
-                    }
-                }
-                if (shouldSwitch) {
-                  /* If a switch has been marked, make the switch
-                  and mark that a switch has been done: */
-                  rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-                  switching = true;
-                  // Each time a switch is done, increase this count by 1:
-                  switchcount ++;
-                } else {
-                  /* If no switching has been done AND the direction is "asc",
-                  set the direction to "desc" and run the while loop again. */
-                  if (switchcount == 0 && dir == "asc") {
-                    dir = "desc";
-                    switching = true;
-                  }
-                }
-            }
-        }
 
         $rootScope.$on('http.error', function (event, status) {
             switch (status) {
